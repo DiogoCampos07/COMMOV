46060242b6ad6dedca7b57d80bb7e068fe0bc502
Diogo Campos <83606982+DiogoCampos07@users.noreply.github.com>
2023-02-17 12:08:23 +0000

    B1

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -12,7 +12,7 @@ class MainActivity : AppCompatActivity() {
     // Está foi a minha segunda alteração
     // Está foi a minha terceira alteração
     // Sou o chico da tina e mudei isto
-    //b1.1
+    //b1
     //b1
     //b1
 
